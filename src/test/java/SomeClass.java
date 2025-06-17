4e1b043adc8c955f3965e78ae3540961b416a6c9
HubertPogonowski <u56649@student.wat.edu.pl>
2025-06-17 17:50:26 +0200

    init

---
+++ src/test/java/SomeClass.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+public class SomeClass {
+    @Test
+    public void test1() {
+        int value = 2;
+        int value2 = 3;
+        assert value2 + value == 5;
+    }
+
+    @Test
+    public void test2() {
+        int x = 2;
+        int y = 3;
+        assert x != y;
+    }
+}
