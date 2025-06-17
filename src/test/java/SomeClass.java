69595e58dda9eb464a714b198388f37ef758dab3
HubertPogonowski <u56649@student.wat.edu.pl>
2025-06-17 21:25:38 +0200

    Update SomeClass.java

--- src/test/java/SomeClass.java
+++ src/test/java/SomeClass.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 public class SomeClass {
+
     @Test
     public void test1() {
         int value = 2;
@@ -13,5 +14,33 @@ public class SomeClass {
         int x = 2;
         int y = 3;
         assert x != y;
+    }
+
+    @Test
+    public void testWithEmptyCatch() {
+        try {
+            int a = 1 / 0;
+        } catch (Exception e) {
+            // Violation: empty catch block
+        }
+    }
+
+    @Test
+    public void testWithPrint() {
+        System.out.println("This should be logged properly"); // Violation: use logger instead
+    }
+
+    @Test
+    public void duplicatedLogic1() {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+    }
+
+    @Test
+    public void duplicatedLogic2() {
+        int a = 10;
+        int b = 20;
+        int sum = a + b; // Violation: duplicated code
     }
 }
